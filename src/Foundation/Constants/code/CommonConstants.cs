3ac505243bf6d554a228d58fe669b5771524ed41
Nelson Joel Raj Jesudos <njesudos@horizontalintegration.com>
2019-03-13 14:23:28 +0400

    Added home page imageText 3 variants content pages item(Oe time checkin till production)

---
+++ src/Foundation/Constants/code/CommonConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMAAR.ECM.Foundation.Constants
+{
+    public static class CommonConstants
+    {
+        public  enum Alignment { Left,Right,Background };
+    }
+}
