3c22509c95d750e85864d548fcbbad17227903c6
Nelson Joel Raj Jesudos <njesudos@horizontalintegration.com>
2019-03-13 17:05:33 +0400

    updated comments

--- src/Feature/Banner/code/Interfaces/IBannerRepository.cs
+++ src/Feature/Banner/code/Interfaces/IBannerRepository.cs
@@ -8,6 +8,10 @@ namespace EMAAR.ECM.Feature.Banner.Interfaces
     /// </summary>
     public interface IBannerRepository
     {
+        /// <summary>
+        /// Getting 3 variants of ImageText components(Left,Right and Background)
+        /// <param name="alignment">Variant</param>
+        /// <returns>ImageText component variation based on parameter selected from Sitecore</returns>
         IImageText GetBannerVariants(out Alignment alignment);
     }
 }
