1af6015fcd6ecdee691dc020d0b9c0da900823c4
G Naresh Kumar <ngeepalem@sapient.com>
2019-04-15 23:48:10 +0400

    Latest assets

--- src/Project/Website/code/Assets/Project/ECM/js/Application/app.js
+++ src/Project/Website/code/Assets/Project/ECM/js/Application/app.js
@@ -105,7 +105,7 @@ var getData = (function ($) {
                     template2 = Handlebars.compile(source2);
                     
                     $('.spinner').remove();
-                    for (var j = 0; dataPageSize > j && j <= data.results.Totalcount; j++){
+                    for (var j = 0; dataPageSize > j && j <= data.results.Totalcount && j < data.results.results.length; j++){
                         $(resultDiv).append(template2(data.results.results[j]));
                     }
                     
@@ -166,7 +166,7 @@ var getData = (function ($) {
                     
                     $('.spinner').remove();
                     
-                    for (var j = 0; j < (dataPageSize * (parseInt(pageNumber)+1)) && j < data.results.Totalcount; j++){
+                    for (var j = 0; j < data.results.Totalcount && j < data.results.results.length; j++){
                         $(resultDiv).append(template2(data.results.results[j]));
                     }
                     _fancyboxImage();
@@ -239,9 +239,11 @@ var getData = (function ($) {
                         var source2 = $(galleryTemp).html(),
                             template2 = Handlebars.compile(source2);
                             $('.spinner').remove();
-                            for (var j = 0; j < data.results.Totalcount && j < dataPageSize; j++){
+                            
+                            for (var j = 0; j < data.results.Totalcount && j < dataPageSize && j < data.results.results.length; j++){
                                 $(resultDiv).append(template2(data.results.results[j]));
                             }
+                            
                             if (data.results != null && (dataPageSize * (pageNumber + 1)) < data.results.Totalcount) {
                                 $(".loadmore").css({'display': 'inline-block'});
                                 // $(".loadmore").attr({ "data-pagenumber": 1 });
